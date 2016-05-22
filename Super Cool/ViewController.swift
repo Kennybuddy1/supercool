40feae1a2362b691b9a0b611112c286b628635f4
Kabir arora <Kabirarora@iMac.local>
2016-05-22 12:18:25 -0400

    added files for super cool

--- Super Cool/ViewController.swift
+++ Super Cool/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var backround: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +22,11 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func button2(sender: AnyObject) {
+        logo.hidden = false
+        backround.hidden = false
+        button.hidden = true
     }
 
 
